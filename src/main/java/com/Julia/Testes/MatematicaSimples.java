97c1a6ba65a5b73586d8a05ee1d8c4ebd14eedbd
juliac-jpg <juliafcostcds@gmail.com>
2026-04-09 22:00:37 -0300

    aula prática de testes de sistemas 09/abr

---
+++ src/main/java/com/Julia/Testes/MatematicaSimples.java
@@ -0,0 +1,20 @@
+package com.Julia.Testes;
+
+public class MatematicaSimples {
+
+    public Double sum(Double firstNumber, Double secondNumber){
+        return firstNumber + secondNumber;
+    }
+
+    public Double sub(Double firstNumber, Double secondNumber){
+        return firstNumber - secondNumber;
+    }
+
+    public Double mult(Double firstNumber, Double secondNumber){
+        return firstNumber * secondNumber;
+    }
+
+    public Double div(Double firstNumber, Double secondNumber){
+        return firstNumber/secondNumber;
+    }
+}
---
+++ src/test/java/com/Julia/Testes/MatematicaSimplesTest.java
@@ -0,0 +1,14 @@
+package com.Julia.Testes;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+
+@DisplayName(("Testes de Matemática com operações simples"))
+public class MatematicaSimplesTest {
+
+    MatematicaSimples math;
+
+    @BeforeAll
+
+
+}
